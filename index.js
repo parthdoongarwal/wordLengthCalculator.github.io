43d8c5c0afd80e3ab9e56c4e32e7582923eb24a0
Parth Doongarwal <77840382+parthdoongarwal@users.noreply.github.com>
2021-09-10 11:46:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const input = document.getElementById("input");
 const root = document.getElementById("root");
+
+input.addEventListener("keypress", (e) => e.key === "Enter" && main());
+
 function main() {
   let inputValue = input.value;
   let string = inputValue.toString();
