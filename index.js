2c0b030485af4f10c51a92c6d79cad7a40773ffb
Parth Doongarwal <77840382+parthdoongarwal@users.noreply.github.com>
2021-09-10 11:20:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const input = document.getElementById("input");
 const root = document.getElementById("root");
 function main() {
-  root.innerText = `Number of Letters: ${input.value.length}`;
+  let inputValue = input.value;
+  let string = inputValue.toString();
+
+  const removeSpaces = (str) => str.replace(/\s/g, "");
+
+  let idk = removeSpaces(string);
+  console.log(idk);
+  root.innerText = `Number of Letters: ${idk.length}`;
 }
